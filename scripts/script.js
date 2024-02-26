c70151364ec1da26446bb40a699fa4ef4edc982d
Pavlo <hit.pasha@gmail.com>
2024-02-26 20:15:10 +0200

    mobile modal menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 function menuOpen() {
     var hamburger = document.getElementById("hamburger");
+    var modalMenu = document.getElementById("modalMenu");
     hamburger.classList.toggle("open");
+    modalMenu.classList.toggle("show");
 }
