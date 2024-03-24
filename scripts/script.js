2ecd6e563f31531f130504d2c7e717820a934fe2
Pavlo <hit.pasha@gmail.com>
2024-03-24 16:54:09 +0200

    working form, start of filtration

--- scripts/script.js
+++ scripts/script.js
@@ -13,35 +13,66 @@ form.addEventListener(
     function (event) {
         var formData = new FormData(form);
         var id = formData.get("user_email");
+        var dataSet = [];
 
-        var user_password = formData.get("user_password");
-        var user_name = formData.get("user_name");
-        var user_tel = formData.get("user_tel");
-        var user_time = formData.get("user_time");
-        var user_dateOfBirth = formData.get("user_dateOfBirth");
-        var user_gender = formData.get("user_gender");
-        var user_countries = formData.get("user_countries");
-        var user_transport = formData.getAll("user_transport");
+        for (var i of formData.entries()) {
 
-        var array = [
-            '{"user_password": "' + user_password + '", ' +
-            '"user_name": "' + user_name + '", ' +
-            '"user_tel": "' + user_tel + '", ' +
-            '"user_time": "' + user_time + '", ' +
-            '"user_dateOfBirth": "' + user_dateOfBirth + '", ' +
-            '"user_gender": "' + user_gender + '", ' +
-            '"user_countries": "' + user_countries + '", ' +
-            '"user_transport": [' + user_transport + ']} '
-        ];
+            dataSet.push('"' + i[0] + '":' + '"' + i[1] + '"');
+        }
+        var dataString = dataSet.toString();
 
-        localStorage.setItem(id, array);
+        localStorage.setItem(id, "{" + dataString + "}");
+
+
+        console.log(JSON.parse(localStorage.getItem(id)));
+        console.log(allStorage())
+        //console.log(JSON.parse("{" + dataString + "}"));
+        /*     var id = formData.get("user_email");
+               var user_password = formData.get("user_password");
+               var user_name = formData.get("user_name");
+               var user_tel = formData.get("user_tel");
+               var user_time = formData.get("user_time");
+               var user_dateOfBirth = formData.get("user_dateOfBirth");
+               var user_gender = formData.get("user_gender");
+               var user_countries = formData.get("user_countries");
+               var user_transport = formData.getAll("user_transport");
+       
+               var dataSet = [
+                   '{user_password: "' + user_password + '", ' +
+                   'user_name: "' + user_name + '", ' +
+                   'user_tel: "' + user_tel + '", ' +
+                   'user_time: "' + user_time + '", ' +
+                   'user_dateOfBirth: "' + user_dateOfBirth + '", ' +
+                   'user_gender: "' + user_gender + '", ' +
+                   'user_countries: "' + user_countries + '", ' +
+                   'user_transport: [' + user_transport + ']} '
+               ];
+       
+               localStorage.setItem(id, dataSet);
+               console.log(id + ': ' + localStorage.getItem(id));*/
 
         event.preventDefault();
     },
     false
 );
 
-/*
-for (var i of formData.entries()) {
-  console.log(i[0] + "; " + i[1]);
-}*/
+function allStorage() {
+
+    var archive = [],
+        keys = Object.keys(localStorage),
+        i = 0, key;
+
+    for (; key = keys[i]; i++) {
+        archive.push(JSON.parse(localStorage.getItem(key)));
+    }
+
+    return archive;
+}
+function filterByGender(gender) {
+    var data = allStorage()
+    data.forEach(func => {
+        if (func.user_gender === gender) {
+            alert(JSON.stringify(func))
+        }
+    })
+}
